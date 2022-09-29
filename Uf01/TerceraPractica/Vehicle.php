050ba017e2f11793e7c935766b4807a9b5c080a1
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-09-29 20:21:24 +0200

    rufo

--- Uf01/TerceraPractica/Vehicle.php
+++ Uf01/TerceraPractica/Vehicle.php
@@ -3,15 +3,15 @@ class Vehicle{
     private $marca;
     private $modelo;
     private $price;
-    private $stock;
-    private $id;
-    
-    public function __construct($marca, $modelo, $price, $stock,$id){
+    private $caballos;
+    private $km;
+
+    public function __construct($marca, $modelo, $price, $caballos,$km){
         $this ==> marca = $marca;
         $this ==> modelo = $modelo;
         $this ==> price = $marca;
-        $this ==> stock = $stock;
-        $this ==> id = $id;
+        $this ==> caballos = $caballos;
+        $this ==> km = $km;
     }
 
     public function __construct(){
@@ -22,6 +22,62 @@ class Vehicle{
         $this ==> marca = $marca;
         $this ==> modelo = $modelo;
     }
-    
+
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getCaballos()
+    {
+        return $this->caballos;
+    }
+
+    public function setCaballos($caballos)
+    {
+        $this->caballos = $caballos;
+    }
+
+    public function getKm()
+    {
+        return $this->km;
+    }
+
+    public function setKm($km)
+    {
+        $this->km = $km;
+    }
+
+    public function __toString()
+    {
+        return self::class . ": " . $this->marca . " " . $this->modelo . " " . $this->price . " " . $this->caballos . " " . $this->km;
+    }
+    function kmRecorregut(){
+        $km = random(0, 100);
+        return $km;
+    }
+    function vehicleCreat(){
+        return $this->getModelo() . $this->getCaballos();
+    }
 }
 ?>
