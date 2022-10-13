66cc4c7959fde798320f3c94c6752b6b0f5c4779
[emirand20] <[emirand20@jaumebalmes.net]>
2022-10-13 20:49:56 +0200

    practica4

---
+++ Uf01/CuartaPractica/paginaEliminar.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title>Pagina de eliminación</title>
+    </head>
+    <body>
+        <h1>La fila ha sido eliminada con éxito</h1>
+        <form method="get"><input type="submit" name="botonretorno" class="btn btn-outline-primary">Volver</input></form>
+        <?php if(isset($_GET["botonretorno"])){
+                header("Location: index.php");
+            }
+      ?>
+    </body>
+</html>
