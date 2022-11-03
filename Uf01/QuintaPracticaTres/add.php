e8fa06808971fa2b203ff9cccf6e3ca7ab9b424e
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-11-03 20:49:07 +0100

    add2

--- Uf01/QuintaPracticaTres/add.php
+++ Uf01/QuintaPracticaTres/add.php
@@ -1,67 +1,30 @@
-<?php
+/*<?php
 // 1-Conexion con PDO
 try {
     //Connexió a la BBDD
     $myCon = new PDO('mysql:host=localhost; dbname=products', 'root', '');
-    // 2 - SQL INSERT
-    //$sql = 'INSERT INTO products ( Name, Description, price, NumId, q_sold) VALUES ( ?, ?, ?, ?,? )';
- 
  } catch (PDOException $e) {
     echo "error de connexió: " . $e->getMessage() . "<br/>";
     die();
  }
 
-$productSaved = FALSE;
 
-// Comprueba si llegan los datos por post
+//Comprobamos de que llegan los datos del POST
+
 if (isset($_POST['add_product'])){
     $nom = $_POST['name'];
     $descripcio = $_POST['description'];
     $preu = $_POST['price'];
-    $qsold = $_POST['qsold'];
+    $q = $_POST['quantity'];
     $numID = random_int(0, 1000);
-
-    //Validate posted values.
-    if (empty($nom)) {
-        $errors[] = 'Please provide a product name.';
-    }
-
-    if (empty($descripcio)) {
-        $errors[] = 'Please provide a description.';
-    }
-
-    if (empty($preu)) {
-        $errors[] = 'Please provide a description.';
-    }
-
-    if ($qsold == 0) {
-        $errors[] = 'Please provide the q_spld.';
-    }
-
-    if ($numID == 0) {
-        $errors[] = 'Please provide the NumId.';
-    }
-
-    if (!isset($errors)) {
-
-        $sql = 'INSERT INTO products ( Name, Description, price, NumId, q_sold) VALUES ( ?, ?, ?, ?,? )';
-
-        $statement = $connexion->prepare($sql);
-
-
-        $statement->bind_param('sis', $nom, $descripcio,$preu, $qsold, $numID);
-
-
-        $statement->execute();
-
-
-        $lastInsertId = $connexion->insert_id;
-
-
-        $statement->close();
-
-        $productSaved = TRUE;
-
-    }
+    
+    
+    //Hacemos consulta de SQL
+    $consulta = "INSERT INTO product(NumID, Name, Description, Price, Quantity) VALUES ('[$numID]', '[$nom]', '[$descripcio]', '[$preu]', '[$q]')"; 
+    
+    //Usamos el queri
+    $consulta = $myCon()->query($sql);
+    
+    header("Location: index.php");
 }
 ?>
